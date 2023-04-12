33ea1f9483876c3cc0ddba42d166a5fc8ea5bf30
madhuri <madhuribisht46@gmail.com>
2023-04-12 12:49:31 +0530

    czdas

---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main(){
+runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create:(context) => MyAppState(),
+      child:MaterialApp(
+        title: 'First App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange)
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    var appstate=context.watch<MyAppState>();
+    var pair=appstate.current;
+    IconData icon;
+    if(appstate.favourite.contains(pair)){
+      icon=Icons.favorite;
+
+    }
+    else{
+      icon=Icons.favorite_border;
+    }
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('A Random Awesome Idea:'),
+            BigCard(pair: pair),
+            SizedBox(height: 10),
+            Row(
+mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton.icon(onPressed: () {appstate.toggleFavourite();}, icon: Icon(icon), label: Text('Like')),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    appstate.getNext();
+                  },
+                  child: Text('Next'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme=Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(pair.asLowerCase,style: style,),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current=WordPair.random();
+
+  void getNext(){
+    current=WordPair.random();
+    notifyListeners();
+  }
+
+  var favourite=<WordPair>[];
+  void toggleFavourite(){
+    if(favourite.contains(current)){
+      favourite.remove(current);
+
+    }
+    else {
+      favourite.add(current);
+    }
+    notifyListeners();
+  }
+}
